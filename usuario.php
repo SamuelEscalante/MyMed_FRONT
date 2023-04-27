e83635efa52122763d5f4447f685d296de7007ce
saalesgu <saalesgu@gmail.com>
2023-04-27 10:36:52 -0500

    imagen en usuario

--- usuario.php
+++ usuario.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <title>Document</title>
-</head>
-
 <body>
     <?php
     session_start();
@@ -41,8 +29,13 @@ navigation">
         </div>
     </nav>
     <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <img src="https://i0.wp.com/pymesenlaweb.com/wp-content/uploads/2019/12/consulta-gratis-scaled.jpg?resize=1024%2C443&ssl=1" class="img-fluid" alt="Consulta Gratis">
+            </div>
+        </div>
         <h1>Bienvenido a YOU MED</h1>
-        <p>Seleccione una opción en la barra de navegación para
-            comenzar.</p>
-    </div>
+        <p>Seleccione una opción en la barra de navegación para comenzar.</p>
+    </div>
 </body>
+
