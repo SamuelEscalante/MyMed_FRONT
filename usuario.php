6e33e64e13ee3fdb4e6577437f5a6ee612e0d4a9
saalesgu <saalesgu@gmail.com>
2023-05-04 22:03:35 -0500

    mejora visual usuario.php

--- usuario.php
+++ usuario.php
@@ -18,34 +18,34 @@
         header("Location: index.html");
     }
     ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">YOU MED</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle
-navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="user-compras.php">Comprar Medicamentos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="user-medic.php">Mis Compras</a>
-                    </li>
-                </ul>
-                <span class="navbar-text">
-                    Bienvenido, <?php echo $us; ?>
-                </span>
-            </div>
-        </div>
-    </nav>
-    <div class="container mt-4">
-        <h1>Bienvenido a YOU MED</h1>
-        <p>Seleccione una opción en la barra de navegación para
-	    comenzar.</p>
-<div class="text-center">
-        <img src="https://i0.wp.com/pymesenlaweb.com/wp-content/uploads/2019/12/consulta-gratis-scaled.jpg?resize=1024%2C443&ssl=1" style="width: 900px; height: 300px;">
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.html">YOU MED</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="user-compras.php">Comprar Medicamentos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="user-medic.php">Mis Compras</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+        Bienvenido, <?php echo $us; ?>
+      </span>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-4">
+  <h1 style="text-align:center; font-weight:bold; font-size:40px;">Bienvenido a YOU MED</h1>
+  <p style="text-align:center; font-size:20px;">Seleccione una opción en la barra de navegación para comenzar.</p>
+  <div class="d-flex justify-content-center mt-4">
+    <img src="https://i0.wp.com/pymesenlaweb.com/wp-content/uploads/2019/12/consulta-gratis-scaled.jpg?resize=1024%2C443&ssl=1" style="max-width: 100%; height: auto;">
+  </div>
+</div>
  </div>
 </body>
